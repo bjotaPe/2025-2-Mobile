d964e63e1ac8ee41fe6f9e83151fe9aee414436c
João Pedro <166267171+bjotaPe@users.noreply.github.com>
2025-09-04 00:58:59 +0000

    feat: if staement

--- main.dart
+++ main.dart
@@ -1,17 +1,15 @@
 import 'dart:io';
 void main(){
+    double renda = 6000;
+    double saldo = 100001;
 
-    int idade = 17;
-    // if(idade >= 18)
-    // {
-    //    print('pode entrar na balada');
-    // }
-    // print('fim do programa');
-    
-    if(idade < 18)
-    {
-        print('fim do programa');
-        return;
-    }
-    print('pode entrar na balada');
+    if(renda > 1400 && renda < 5000){
+        print('varejo');
+    }else if(renda < 5000 && renda > 30000){
+        print('Vangogh');
+    }else if(renda < 30000 && renda > 60000){
+        print('Select');
+    }else if(renda > 60000 && saldo > 100000){
+        print('Exclusive');
+    };
 }
